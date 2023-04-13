4218dc07b80ef41d47e708c24715e6e3fa5899ec
root <kppoku@gmail.com>
2023-04-13 12:41:56 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    int i;
+	int len, i;
+	uint sum = 0;
 
-    if (b == NULL)
-        return 0;
+	if (!b)
+		return (sum);
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] == '0')
-            result = (result << 1);
-        else if (b[i] == '1')
-            result = (result << 1) | 1;
-        else
-            return 0;
-    }
+	len = slen(b);
+	if (!valid(b))
+		return (0);
 
-    return result;
+	for (i = 0, len--; b[i]; i++, len--)
+		if (b[i] == '1')
+			sum += 1 * (1 << len);
+
+	return (sum);
 }
