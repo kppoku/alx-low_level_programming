a10c48acc118177dd4044d9b6b3d3d74ced369a0
Your Name <kppoku@gmail.com>
2023-05-04 17:53:32 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,7 +4,7 @@
 /**
  * flip_bits - flip bits to convert one number to another number
  * @n: first number
- * @m: second number to convert to
+ * @m: second number to convert
  * Return: number of bits that was needed to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
